0935f4e0a86449d30cbaafbe7e47c0040daa49dc
Jai Aakaah K <jaiaakaashoffici@gmail.com>
2026-02-02 15:21:33 +0530

    Update sobel_output.py

--- sobel_output.py
+++ sobel_output.py
@@ -1,9 +1,5 @@
 from PIL import Image
-
-# Load PGM image
 img = Image.open("sobel_output_4.pgm")
-
-# Save as JPG
 img.save("sobel_output_4.jpg")
-
 print("Converted sobel_output.pgm to sobel_output.jpg")
+
